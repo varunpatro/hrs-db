ebea3b76bb34410d1b1ddc40be0ac8ef193ee473
Varun Patro <varunpatro@u.nus.edu>
2014-12-23 17:29:03 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var fs = require('fs');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser());	
+
+app.get('/', function (req, res) {
+	res.sendfile('form.html');
+});
+
+app.post('/post', function (req, res) {
+	fs.readFile('data.json', function (err, data) {
+		var to_store = JSON.parse(data);
+		if (to_store[req.body.block] === undefined) {
+			to_store[req.body.block] = {};
+		}
+		(to_store[req.body.block])[req.body.flat] = req.body.tel;
+
+		fs.writeFile('data.json',  JSON.stringify(to_store), function (err) {
+		  if (err) throw err;
+		});
+		
+	});
+	res.sendfile('confirm.html');
+});
+
+
+app.get('/get', function (req, res) {
+	res.sendfile('data.json');
+});
+
+
+app.listen(3000);
